5a2a3bd5945e0ae5bd5d782d48e18d9025aceb0b
LinLingshuang <2025302114225@whu.edu.cn>
2026-04-11 20:09:15 +0800

    提取出神经网络类

--- scr/Matrix.h
+++ scr/Matrix.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include<cmath>
+#include <fstream>
 #include "VectorD.h"
 class Matrix {
 public:
@@ -92,3 +92,100 @@ private:
 	int rowNum;
 	int columnNum;
 };
+
+// Matrix calculations //
+Matrix multiplication(Matrix A, Matrix B) {
+	if (A.getcolumnNum() != B.getrowNum()) {
+		Matrix C = Matrix();
+		return C;
+	}
+	Matrix C = Matrix(A.getrowNum(), B.getcolumnNum());
+	for (int i = 0; i < A.getrowNum(); i++) {
+		for (int j = 0; j < B.getcolumnNum(); j++) {
+			double c = 0;
+			for (int k = 0; k < A.getcolumnNum(); k++) {
+				c += (A.getVectorD()[k][i] * B.getVectorD()[j][k]);
+			}
+			C.setValue(i + 1, j + 1, c);
+		}
+	}
+	return C;
+}
+Matrix multiplicationOneByOne(Matrix A, Matrix B) {
+	if (A.getcolumnNum() != B.getcolumnNum() || A.getrowNum() != B.getrowNum()) {
+		Matrix C = Matrix();
+		return C;
+	}
+	Matrix C = Matrix(A.getrowNum(), B.getcolumnNum());
+	for (int i = 0; i < A.getrowNum(); i++) {
+		for (int j = 0; j < B.getcolumnNum(); j++) {
+			double c = 0;
+			c += (A.getVectorD()[j][i] * B.getVectorD()[j][i]);
+			C.setValue(i + 1, j + 1, c);
+		}
+	}
+	return C;
+}
+Matrix addition(Matrix A, Matrix B) {
+	if (A.getcolumnNum() != B.getcolumnNum() || A.getrowNum() != B.getrowNum()) {
+		Matrix C = Matrix();
+		return C;
+	}
+	Matrix C = Matrix(A.getrowNum(), B.getcolumnNum());
+	for (int i = 0; i < A.getrowNum(); i++) {
+		for (int j = 0; j < B.getcolumnNum(); j++) {
+			double c = 0;
+			c += (A.getVectorD()[j][i] + B.getVectorD()[j][i]);
+			C.setValue(i + 1, j + 1, c);
+		}
+	}
+	return C;
+}
+Matrix subtraction(Matrix A, Matrix B) {
+	if (A.getcolumnNum() != B.getcolumnNum() || A.getrowNum() != B.getrowNum()) {
+		Matrix C = Matrix();
+		return C;
+	}
+	Matrix C = Matrix(A.getrowNum(), B.getcolumnNum());
+	for (int i = 0; i < A.getrowNum(); i++) {
+		for (int j = 0; j < B.getcolumnNum(); j++) {
+			double c = 0;
+			c += (A.getVectorD()[j][i] - B.getVectorD()[j][i]);
+			C.setValue(i + 1, j + 1, c);
+		}
+	}
+	return C;
+}
+Matrix transpose(Matrix A) {
+	Matrix C = Matrix(A.getcolumnNum(), A.getrowNum());
+	for (int i = 0; i < A.getrowNum(); i++) {
+		for (int j = 0; j < A.getcolumnNum(); j++) {
+			C.setValue(j + 1, i + 1, A.getVectorD()[j][i]);
+		}
+	}
+	return C;
+}
+Matrix readTxtToMatrix(const string& filepath) {
+	ifstream file(filepath);
+	if (!file.is_open()) {
+		cerr << "无法打开文件: " << filepath << endl;
+		return Matrix(32 * 32, 1, 0.0);
+	}
+	Matrix X(32 * 32, 1, 0.0);
+	string line;
+	int row = 0;
+	while (getline(file, line) && row < 32) {
+		// 假设每行32个字符，无空格
+		for (int col = 0; col < 32 && col < line.length(); col++) {
+			char ch = line[col];
+			double val = (ch == '1') ? 1.0 : 0.0;
+			//cout << val;
+			// 计算在X中的索引：行优先
+			int idx = row * 32 + col;  // 0-based
+			X.setValue(idx + 1, 1, val);  // setValue要求索引从1开始
+		}
+		//cout << endl;
+		row++;
+	}
+	return X;
+}
